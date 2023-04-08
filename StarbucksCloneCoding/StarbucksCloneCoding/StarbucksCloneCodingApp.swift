2cf659adae37e2595d6029d0c54fd0076a0e2b60
yeeton37 <lyeeun37@naver.com>
2023-04-08 17:09:39 +0900

    feat: 폴더 생성

---
+++ StarbucksCloneCoding/StarbucksCloneCoding/StarbucksCloneCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksCloneCodingApp.swift
+//  StarbucksCloneCoding
+//
+//  Created by 이예은 on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksCloneCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
